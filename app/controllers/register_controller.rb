ae246ac07f82f2a7e8b1a32bc379b0726ac4d00b
Senyak <mselina@sfedu.ru>
2022-12-16 02:59:03 +0300

    Editing registration, but it still not work

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -6,7 +6,23 @@ class RegisterController < ApplicationController
   end
 
   def create
+    user = User.new(params[:user])
+    if user.save
+      redirect_to '/'
+    else
+      flash[:error] = user.errors.full_messages
+      redirect_to register_path
+    end
 
+
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email,
+                                 :password, :password_confirmation)
 
   end
 
