668c275d1755b33ea1aa110c06aac21ddc435f0c
Senyak <mselina@sfedu.ru>
2022-12-28 18:51:05 +0300

    Edit migration

--- db/migrate/20221228134321_create_information.rb
+++ db/migrate/20221228134321_create_information.rb
@@ -3,6 +3,8 @@ class CreateInformation < ActiveRecord::Migration[7.0]
     create_table :information do |t|
       t.integer :wins
       t.integer :losses
+      t.integer :user_id
+      t.integer :rating_id
 
       t.timestamps
     end
